1e5a8f194b3f1489daa94eeeb55fbcfa20c5f78f
Amzad Mojumder <amzadfof@gmail.com>
2015-04-28 11:03:55 +0100

    Create htopInstall.sh

---
+++ htopInstall.sh
@@ -0,0 +1,9 @@
+
+
+#http://www.dreamcreative.net/how-tos/guides-server-maintenance/install-htop-in-centos/
+
+
+wget http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm
+rpm -Uhv rpmforge-release*.rf.x86_64.rpm
+
+yum install htop
